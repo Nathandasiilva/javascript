70216042f2a19d29fe4ba3df88cc8c92006688b8
Nathan Da Silva <117369146+Nathandasiilva@users.noreply.github.com>
2023-03-08 23:34:00 -0300

    Condições parte 1

---
+++ aula011/aula08j.js
@@ -0,0 +1,6 @@
+var vel = 59.2
+console.log(`A velocidade do seu caro é ${vel}km/h`)
+if (vel > 60) { //Condição simples!
+    console.log(`Você ultrapassou o limite da via. MULTADO`)
+}
+console.log(`Diriga sempre com o sito de seguração`)
