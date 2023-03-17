98e2c82e00ebd88011f8c7d7a10c5810ba1d4932
Nathan Da Silva <117369146+Nathandasiilva@users.noreply.github.com>
2023-03-16 23:28:28 -0300

    Repetição parte 2

---
+++ aula014/ambiente.js
@@ -0,0 +1,36 @@
+console.log('Vai começar...')
+for (var c = 1; c <= 5; c++){
+    console.log(c)
+}
+console.log('Fim!!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+for (var c=1; c <= 10; c++) {
+    console.log(c)
+} 
+*/
+    
+
+
+/*
+var c = 1 
+while (c <= 10) {
+    console.log(c)
+    c++
+}  
+*/
